4be9e79d128e7fa6f01d51065a18ec9c3e5c5e84
Marie-Garde <marie_garde@ymail.com>
2025-05-14 16:24:45 +0200

    change source owl image

--- src/components/Presentation.vue
+++ src/components/Presentation.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 import backgroundImage from '@/assets/illustration/background-presentation.png';
 import backgroundMobileImage from '@/assets/illustration/background-presentation-mobile.png';
-import owls from '@/assets/illustration/chouettes-colorées.png';
+import owls from '@/assets/illustration/chouettes-couleur-blanc.png';
 import { isMobile } from '@/reactives/isMobile';
 
 const mobile = isMobile();
